654302e30eb28f752365f23e7902196c2ba4b1fc
wretchedghost <34467228+wretchedghost@users.noreply.github.com>
2022-01-06 09:10:01 -0700

    Blocked notifications when locked
    Added a pkill to dunst when locking and renabling it when unlocked

--- config/i3/lock_and_blur.sh
+++ config/i3/lock_and_blur.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# suspend message display: dunst
+pkill -u "$USER" -USR1 dunst
+
+## Lock the screen 
 # set the icon and a temporary location for the screenshot to be stored
 icon="$HOME/.config/i3/lock-icon-light.png"
 tmpbg='/tmp/screen.png'
@@ -14,8 +18,12 @@ convert "$tmpbg" -filter Gaussian -thumbnail 20% -sample 500% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite "$tmpbg"
 
 # lock the screen with the blurred screenshot
-i3lock -i "$tmpbg"
+# -n is required for not forking
+i3lock -ni "$tmpbg"
 
 rm $tmpbg
+
+# resume message display when unlocked
+pkill -u "$USER" -USR2 dunst
 
 exit 0
